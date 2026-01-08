44747c12b39ee1a202e8140e8e72fe04cde8141f
Arvv-Institucional <up240589@alumnos.upa.edu.mx>
2026-01-08 12:16:12 -0600

    tarea Estructura de datos terminada

--- Java/Estructura datos/1.1 Arreglos y matrices acercamiento poo/src/Calificaciones.java
+++ Java/Estructura datos/1.1 Arreglos y matrices acercamiento poo/src/Calificaciones.java
@@ -13,14 +13,39 @@ class Calificaciones {
     for(int a=0; a<calificaciones[persona].length; a++) {
       promPersona = promPersona + calificaciones[persona][a];
     }
-    promPersona=promPersona/4;
+    promPersona=promPersona/calificaciones[persona].length;
     return promPersona;
   }
 
-  public void ImprimirTabla() {
-    for(int i=0; i<materias.length; i++) {
-      System.out.print("         " + materias[i]);
+  public double PromMateria(int materia) {
+    double promMateria=0;
+    for(int a=0; a<calificaciones[materia].length; a++) {
+      promMateria = promMateria + calificaciones[a][materia];
     }
-    
+    promMateria=promMateria/calificaciones[materia].length;
+    return promMateria;
+  }
+
+  public void ImprimirTabla() {
+    System.out.print("     ");
+    for(int i=0; i<materias.length; i++) {
+      System.out.print("      " + materias[i]);
+    }
+    System.out.print("    promedio");
+    for(int p=0; p<materias.length; p++) {
+      System.out.println();
+      /*con String Format se da formato el 8 representa el
+       ancho que requiere la parte que es el espacio de los nombres*/
+      System.out.print(String.format("%8s", nombres[p]));
+      for(int c=0; c<materias.length; c++) {
+        System.out.print("         " + calificaciones[p][c]);
+      }
+      System.out.print("          " + PromPersona(p));
+    }
+    System.out.println();
+    System.out.print("promedio");
+    for(int m=0; m<calificaciones.length; m++) {
+      System.out.print("         " + PromMateria(m));
+    }
   }
 }
