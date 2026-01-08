ec4ac0bd37df2eefa9cabf4e94c336c339c692d9
Arvv-Institucional <up240589@alumnos.upa.edu.mx>
2026-01-08 12:41:37 -0600

    no entiendo fokin paiton

---
+++ Python/Estructura de datos/ArreglosYmatrices.py
@@ -0,0 +1,8 @@
+nombres = ['Ana', 'Luis', 'Carlos', 'Marta']
+materias = ['Matemáticas', 'Física', 'Química', 'Biología']
+calificaciones = [[10, 9, 8, 7],
+                  [9, 7, 6, 8],
+                  [8, 9, 5, 9],
+                  [7, 8, 9, 10]]
+for i in materias:
+  print(f"      {i}")
