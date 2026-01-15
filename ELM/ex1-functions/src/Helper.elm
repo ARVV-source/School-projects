17d2de09578164ca7618ec573a331951ac8a2368
Arvv_institucional <up240589@upa.edu.mx>
2026-01-15 17:25:58 -0600

    Otro dia de Elm

--- ELM/ex1-functions/src/Helper.elm
+++ ELM/ex1-functions/src/Helper.elm
@@ -1,32 +1,74 @@
 module Helper exposing (..)
+
+
 duplic : number -> number
 duplic inputNum =
-  2 * inputNum
+    2 * inputNum
+
 
 alCuad : number -> number
 alCuad num =
-  num^2
+    num ^ 2
+
 
 greet : String -> String
 greet nom =
-  "Hola " ++ nom
+    "Hola " ++ nom
+
 
 above5 : number -> Bool
 above5 num =
-  num > 5
+    num > 5
+
 
 ifBoolTranslate : Bool -> String
 ifBoolTranslate expr =
-  if expr == True then
-    "positivo"
-  else
-    "falso"
+    if expr == True then
+        "positivo"
+
+    else
+        "falso"
+
 
 ifNumberSign : number -> String
 ifNumberSign num =
-  if num > 0 then
-    "positivo"
-  else if num == 0 then
-    "neutral"
+    if num > 0 then
+        "positivo"
+
+    else if num == 0 then
+        "neutral"
+
     else
-    "negativo"
+        "negativo"
+
+
+getNameById : Int -> String
+getNameById id =
+    case id of
+        1 ->
+            "Fernanda"
+
+        2 ->
+            "Miguel"
+
+        3 ->
+            "Juan"
+
+        _ ->
+            "Desconocido"
+
+
+getGradeByName : String -> Float
+getGradeByName name =
+    case name of
+        "Fernanda" ->
+            9.4
+
+        "Miguel" ->
+            8.7
+
+        "Juan" ->
+            9.3
+
+        _ ->
+            0
